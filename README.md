9f16d1054486a64911d0a5343c02ba8b8e71ab02
inniy2 <inniy2@gmail.com>
2020-05-20 16:40:34 +0300

    support all except gh-ost

--- README.md
+++ README.md
@@ -21,6 +21,8 @@ source ~/.bashrc
 go get -u google.golang.org/grpc
 go get -u github.com/golang/protobuf/protoc-gen-go
 
+go get -u github.com/go-sql-driver/mysql
+
 vim ~/.bashrc
 
 export PATH=$PATH:/home/sangsun/go/bin
--- server.go
+++ server.go
@@ -25,37 +25,105 @@ import (
 	"net"
 	"google.golang.org/grpc"
 	pb "./proto"
+	"syscall"
+	"strconv"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"os/exec"
+	"os"
+	"path/filepath"
 )
 
 const (
 	port = ":9090"
 )
 
+type Tag struct {
+    Table   string    `json:"table"`
+    CreateTable string `json:"CreateTable"`
+}
+
+type Tag2 struct {
+    TableRow   string    `json:"TableRow"`
+}
 
 type server struct {
 	pb.UnimplementedGhostServer
 }
 
 func (s *server) Diskcheck(ctx context.Context, in *pb.DiskRequest) (*pb.APIResponse, error) {
-	log.Printf("Received: "+ in.Dir)
-	return &pb.APIResponse{Responsemessage: "Diskcheck ", Responsecode: 0}, nil
+	log.Printf("Diskcheck Received: "+ in.Dir)
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(in.Dir, &fs)
+	if err != nil {
+		return &pb.APIResponse{Responsemessage: "Diskcheck error", Responsecode: 1}, nil
+	}
+	return &pb.APIResponse{Responsemessage: strconv.FormatUint(fs.Bfree * uint64(fs.Bsize) / 1024 / 1024 / 1024, 10)+"G", Responsecode: 0}, nil
 }
 
 func (s *server) Checkdefinition(ctx context.Context, in *pb.DefinitionRequest) (*pb.APIResponse, error) {
-	log.Printf("Received: "+ in.Schemaname)
-	log.Printf("Received: "+ in.Tablename)
-	return &pb.APIResponse{Responsemessage: "Checkdefinition ", Responsecode: 0}, nil
+	log.Printf("Checkdefinition Received: "+ in.Schemaname)
+	log.Printf("Checkdefinition Received: "+ in.Tablename)
+	db, err := sql.Open("mysql", "root:12345678@tcp(10.145.239.38:3306)/"+in.Schemaname)
+	if err != nil {
+        panic(err.Error())
+	}
+	defer db.Close()
+	results, err := db.Query("show create table "+in.Schemaname+"."+in.Tablename)
+	if err != nil {
+        panic(err.Error())
+	}
+	var defintion string = "";
+	for results.Next() {
+        var tag Tag
+        // for each row, scan the result into our tag composite object
+        err = results.Scan(&tag.Table, &tag.CreateTable)
+        if err != nil {
+            panic(err.Error()) // proper error handling instead of panic in your app
+        }
+                // and then print out the tag's Name attribute
+		log.Printf(tag.Table)
+		log.Printf(tag.CreateTable)
+		defintion = tag.CreateTable
+    }
+	defer results.Close()
+	return &pb.APIResponse{Responsemessage: defintion, Responsecode: 0}, nil
 }
 
 func (s *server) Cutover(ctx context.Context, in *pb.Empty) (*pb.APIResponse, error) {
+	err := os.Remove("/tmp/ghost.postpone.flag")
+	if err != nil {
+		return &pb.APIResponse{Responsemessage: "Cutover ", Responsecode: 1}, nil
+	}
 	return &pb.APIResponse{Responsemessage: "Cutover ", Responsecode: 0}, nil
 }
 
 func (s *server) Putpanicflag(ctx context.Context, in *pb.Empty) (*pb.APIResponse, error) {
+	cmd := exec.Command("touch", "/tmp/ghost.panic.flag")
+	log.Printf("Running command touch /tmp/ghost.panic.flag ...")
+	err := cmd.Run()
+	log.Printf("Command finished with code: %v", err)
 	return &pb.APIResponse{Responsemessage: "Putpanicflag ", Responsecode: 0}, nil
 }
 
 func (s *server) Cleanup(ctx context.Context, in *pb.Empty) (*pb.APIResponse, error) {
+	array := [2]string{"/tmp/ghost.panic.flag","/tmp/ghost.postpone.flag"}
+	for i := 0; i < len(array); i++ {
+		err := os.Remove(array[i])
+		log.Printf(array[i])
+		if err != nil {
+			return &pb.APIResponse{Responsemessage: "Cleanup ", Responsecode: 1}, nil
+		}
+	}
+	files, err := filepath.Glob("/tmp/gh-ost.*.*.sock")
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range files {
+		if err := os.Remove(f); err != nil {
+			panic(err)
+		}
+	}
 	return &pb.APIResponse{Responsemessage: "Cleanup ", Responsecode: 0}, nil
 }
 
@@ -83,14 +151,44 @@ func (s *server) Interactive(ctx context.Context, in *pb.InteractiveRequest) (*p
 func (s *server) Rowcount(ctx context.Context, in *pb.DefinitionRequest) (*pb.APIResponse, error) {
 	log.Printf("Received: "+ in.Schemaname)
 	log.Printf("Received: "+ in.Tablename)
-	return &pb.APIResponse{Responsemessage: "Rowcount ", Responsecode: 0}, nil
+	db, err := sql.Open("mysql", "root:12345678@tcp(10.145.239.38:3306)/"+in.Schemaname)
+	if err != nil {
+        panic(err.Error())
+	}
+	defer db.Close()
+	results, err := db.Query("SELECT TABLE_ROWS FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '"+in.Schemaname+"' AND TABLE_NAME = '"+in.Tablename+"'")
+	if err != nil {
+        panic(err.Error())
+	}
+
+	var tableRow string = "";
+	for results.Next() {
+        var tag Tag2
+        // for each row, scan the result into our tag composite object
+        err = results.Scan(&tag.TableRow)
+        if err != nil {
+            panic(err.Error()) // proper error handling instead of panic in your app
+        }
+                // and then print out the tag's Name attribute
+		log.Printf(tag.TableRow)
+		tableRow = tag.TableRow
+    }
+	defer results.Close()
+	return &pb.APIResponse{Responsemessage: tableRow, Responsecode: 0}, nil
 }
 
 func (s *server) Ibdsize(ctx context.Context, in *pb.IbdRequest) (*pb.APIResponse, error) {
 	log.Printf("Received: "+ in.Dir)
 	log.Printf("Received: "+ in.Schemaname)
 	log.Printf("Received: "+ in.Tablename)
-	return &pb.APIResponse{Responsemessage: "Ibdsize ", Responsecode: 0}, nil
+	fi, err := os.Stat(in.Dir+"/"+in.Schemaname+"/"+in.Tablename+".ibd")
+	if err != nil {
+	    panic(err.Error())
+	}
+	// get the size
+	size := fi.Size()
+	log.Printf(strconv.FormatInt(size,10))
+	return &pb.APIResponse{Responsemessage: strconv.FormatInt(size / 1024 / 1024 / 1024,10)+"G", Responsecode: 0}, nil
 }
 
 
